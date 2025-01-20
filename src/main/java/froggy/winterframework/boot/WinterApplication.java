542de021d4d686ac20f66d474b1d7a2b5eec27e3
신성현 <froggy0m0a@gmail.com>
2025-01-20 23:44:00 +0900

    feat : Embedded Jetty 서버 기능 추가
    - Embedded Jetty 서버 설정
        - 기본 포트 : 8080
        - 정적 자원 경로 : src/main/webapp
        - 메인 화면 : index.jsp
    
    - Embeded Jetty 서버관련 의존성 추가
        - JSP, JSTL, Webapp추가
        - Jetty Version을 Maven 프로퍼티로 정의

---
+++ src/main/java/froggy/winterframework/boot/WinterApplication.java
@@ -0,0 +1,44 @@
+package froggy.winterframework.boot;
+
+import froggy.winterframework.web.DispatcherServlet;
+import javax.servlet.Servlet;
+import org.apache.jasper.servlet.JspServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class WinterApplication {
+
+    public static void run(Class<?> runClass) throws Exception {
+        Server server = new Server(8080);
+
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+        context.setResourceBase("src/main/webapp"); // JSP 파일경로
+
+        /*
+        * JSP Configuration
+        * AnnotationConfiguration - 애노테이션 기반의 설정
+        */
+        Configuration.ClassList classList = Configuration.ClassList.setServerDefault(server);
+        classList.addBefore(
+            "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+            "org.eclipse.jetty.annotations.AnnotationConfiguration"
+        );
+
+        // jsp요청을 JspServlet에 매핑
+        context.addServlet(JspServlet.class, "*.jsp");
+        context.setWelcomeFiles(new String[]{"index.jsp"});
+
+        context.addServlet(new ServletHolder(DispatcherServlet.class), "/");
+
+        server.setHandler(context);
+
+        // 서버 시작
+        server.start();
+        System.out.println("WinterFramework Server is running on http://localhost:8080");
+        server.join();
+    }
+
+}
