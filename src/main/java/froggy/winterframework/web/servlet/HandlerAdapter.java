0f485a8271cd9809a9c52dba859cde7404367bbf
신성현 <froggy0m0a@gmail.com>
2025-02-12 22:43:18 +0900

    comment: HandlerAdapter 주석 추가

--- src/main/java/froggy/winterframework/web/servlet/HandlerAdapter.java
+++ src/main/java/froggy/winterframework/web/servlet/HandlerAdapter.java
@@ -4,9 +4,28 @@ import froggy.winterframework.web.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 요청을 처리할 적절한 핸들러 어댑터를 결정하고 실행하는 인터페이스.
+ * 다양한 핸들러를 지원하기 위해 사용됨.
+ */
 public interface HandlerAdapter {
 
+    /**
+     * 주어진 핸들러가 해당 어댑터에서 지원 가능한지 확인.
+     *
+     * @param handler 검사할 핸들러 객체
+     * @return 지원 한다면 {@code true}, 그렇지 않으면 {@code false}
+     */
     boolean supports(Object handler);
 
+    /**
+     * 요청을 처리하고 응답을 생성
+     *
+     * @param request  HTTP 요청 객체
+     * @param response HTTP 응답 객체
+     * @param handler  요청을 처리할 핸들러 객체
+     * @return 처리 결과를 포함한 ModelAndView
+     * @throws Exception 핸들러 실행 중 예외
+     */
     ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception;
 }
--- src/main/java/froggy/winterframework/web/servlet/mvc/method/annotation/DefaultControllerHandlerAdapter.java
+++ src/main/java/froggy/winterframework/web/servlet/mvc/method/annotation/DefaultControllerHandlerAdapter.java
@@ -9,8 +9,17 @@ import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * @Controller 애노테이션을 적용한 핸들러를 실행하는 기본 핸들러 어댑터.
+ */
 public class DefaultControllerHandlerAdapter implements HandlerAdapter {
 
+    /**
+     * 해당 핸들러가 @Controller 여부를 확인하여 지원 여부 결정.
+     *
+     * @param handler 검사할 핸들러 객체
+     * @return 지원 한다면 {@code true}, 그렇지 않으면 {@code false}
+     */
     @Override
     public boolean supports(Object handler) {
         return ((HandlerMethod) handler)
@@ -19,6 +28,15 @@ public class DefaultControllerHandlerAdapter implements HandlerAdapter {
             .isAnnotationPresent(Controller.class);
     }
 
+    /**
+     * 요청을 처리하고 응답을 생성
+     *
+     * @param request  HTTP 요청 객체
+     * @param response HTTP 응답 객체
+     * @param handler  요청을 처리할 핸들러 객체
+     * @return 처리 결과를 포함한 ModelAndView
+     * @throws Exception 핸들러 실행 중 예외
+     */
     @Override
     public ModelAndView handle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
